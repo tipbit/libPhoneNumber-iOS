6f4c18cc519a4ddf20c43144a7bbaeb1183f1a27
Navi Singh <navisingh@gmail.com>
2015-04-09 15:27:52 -0700

    fix compile errors

--- libPhoneNumber/NBPhoneNumberUtil.m
+++ libPhoneNumber/NBPhoneNumberUtil.m
@@ -3779,7 +3779,7 @@ static NSDictionary *DIGIT_MAPPINGS;
         
         if (keepRawInput)
         {
-            phoneNumber.PreferredDomesticCarrierCode = [carrierCode copy];
+            phoneNumber.preferredDomesticCarrierCode = [carrierCode copy];
         }
     }
     
@@ -3995,11 +3995,11 @@ static NSDictionary *DIGIT_MAPPINGS;
     // that we can use the proto-buffer equality method.
     firstNumber.rawInput = @"";
     [firstNumber clearCountryCodeSource];
-    firstNumber.PreferredDomesticCarrierCode = @"";
+    firstNumber.preferredDomesticCarrierCode = @"";
     
     secondNumber.rawInput = @"";
     [secondNumber clearCountryCodeSource];
-    secondNumber.PreferredDomesticCarrierCode = @"";
+    secondNumber.preferredDomesticCarrierCode = @"";
     
     if (firstNumber.extension != nil && firstNumber.extension.length == 0) {
         firstNumber.extension = nil;
